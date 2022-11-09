aba33e333df15711e530ca3ebca555cf694c449b
meesahil7 <meesahil7@gmail.com>
2022-11-09 22:26:09 +0530

    day-2

--- appletv/src/App.js
+++ appletv/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>apple tv</h1>
-   
+      <h1>apple tv</h1>
     </div>
   );
 }

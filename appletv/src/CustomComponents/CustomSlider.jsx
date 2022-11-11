6a61fac49df5b8ca291d515d31c3d93803eecdb9
Rishav Neogi <nrishav31@gmail.com>
2022-11-11 23:17:04 +0530

    tab responsive added

--- appletv/src/CustomComponents/CustomSlider.jsx
+++ appletv/src/CustomComponents/CustomSlider.jsx
@@ -7,36 +7,68 @@ import axios from "axios";
 import { Img } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 const CustomSlider = ({ url }) => {
-    const [data, setData] = useState([]);
-    const [pathurl,setPathurl]=useState("");
-    useEffect(() => {
-        axios.get(url)
-            .then((r) => {
-                setData(r.data);
-                if(url.includes("watchPremiers")){
-                  setPathurl("watchPremiers");
-                }
-            });
-    }, [])
-    return (
-        <div>
+  const [data, setData] = useState([]);
+  const [pathurl, setPathurl] = useState("");
+  const [size, setSize] = useState(window.innerWidth);
+  const [perPage, setPerPage] = useState(5);
+  const updateSize = () =>
+    setSize(window.innerWidth);
+  useEffect(() => (window.onresize = updateSize), []);//1320,1080
+  useEffect(() => {
+    size > 1320 ? setPerPage(5) : size <= 1320 && size > 1080 ? setPerPage(3) : setPerPage(1)
+  }, [size])
+  useEffect(() => {
+    axios.get(url)
+      .then((r) => {
+        setData(r.data);
+        if (url.includes("watchPremiers")) {
+          setPathurl("watchPremiers");
+        }
+        else if (url.includes("latestOriginals")) {
+          setPathurl("latestOriginals");
+        }
+        else if (url.includes("mostPopular")) {
+          setPathurl("mostPopular");
+        }
+        else if (url.includes("futureRelease")) {
+          setPathurl("futureRelease");
+        }
+        else if (url.includes("comedy")) {
+          setPathurl("comedy");
+        }
+        else if (url.includes("drama")) {
+          setPathurl("drama");
+        }
+        else if (url.includes("allFeatureFilms")) {
+          setPathurl("allFeatureFilms");
+        }
+        else if (url.includes("allDramaFilms")) {
+          setPathurl("allDramaFilms");
+        }
+        else if (url.includes("watchPremiers")) {
+          setPathurl("allComedyFilms");
+        }
+      });
+  }, [])
+  return (
+    <div>
       <Splide options={{
-        perPage: 5,
+        perPage: perPage,
         height: '150px',
         rewind: true,
         gap: '10px',
       }}>
         {
-          data.map(({ image,id }) => (
+          data.map(({ image, id }) => (
             <SplideSlide>
               <Link to={`/${pathurl}/${id}`}>
-              <Img src={image} />
+                <Img src={image} />
               </Link>
             </SplideSlide>
           ))
         }
       </Splide>
     </div>
-    )
+  )
 }
 export { CustomSlider }
--- appletv/src/component/nextpage.jsx
+++ appletv/src/component/nextpage.jsx
@@ -3,82 +3,88 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {background, Box,Button,Image, VStack} from "@chakra-ui/react"
+import { background, Box, Button, Image, VStack } from "@chakra-ui/react"
 import { Episode } from './episode'
 import { Cast } from './cast'
 import { Carausel } from './carausel'
-
+import "../Components/herovideo.css"
 export const Nextpage = () => {
-    const [singlepagedata,setSinglepagedata] = useState({})
-    
+    const [singlepagedata, setSinglepagedata] = useState({})
+
     const params = useParams()
-    let id =params.id
-    let category =params.category
+    let id = params.id
+    let category = params.category
 
-const SingleData =()=>{
-axios.get(`https://appletv-server.vercel.app/${category}/${id}`)
-.then((res)=>{setSinglepagedata(res.data)})
-.catch((err)=>{console.log(err)})
-}
-
-
-console.log(singlepagedata.episode)
-useEffect(()=>{
-SingleData()
-
-},[])
-const style={
-    height :"30px",
-    width : "150px",
-background :"white",
-borderRadius :"7px",
-color :"black",
-borderColor :"transparent",
-fontWeight : "600"
-}
-  return (
-   <Box  >
-    <Box  style={{border  : "1px solid transparent" , position:"absolute" ,marginLeft :"100px",
-marginTop:"40px" ,color:"white"
-}}   
-><h2>{singlepagedata.title}</h2></Box>
-{
-    singlepagedata.video ?   <iframe src="https://www.youtube.com/watch?v=ydkhfToAEcQ" allowFullScreen /> :  <Image src={singlepagedata.image} w={"100%"}  />     
-}
+    const SingleData = () => {
+        axios.get(`https://appletv-server.vercel.app/${category}/${id}`)
+            .then((res) => { setSinglepagedata(res.data) })
+            .catch((err) => { console.log(err) })
+    }
 
 
+    console.log(singlepagedata.episode)
+    useEffect(() => {
+        SingleData()
+
+    }, [])
+    const style = {
+        height: "30px",
+        width: "150px",
+        background: "white",
+        borderRadius: "7px",
+        color: "black",
+        borderColor: "transparent",
+        fontWeight: "600"
+    }
+    const src = "tv.mp4"
+    return (
+        <Box  >
+            <Box style={{
+                border: "1px solid transparent", position: "absolute", marginLeft: "100px",
+                marginTop: "40px", color: "white"
+            }}
+            ><h2>{singlepagedata.title}</h2></Box>
+            {/* {
+    singlepagedata.video ?  <video  className="base-img"   autoPlay  playsinline muted preload="auto">
+    <source src={src} type="video/mp4"  />
+  </video>  :  <Image src={singlepagedata.image} w={"100%"}  />     
+} */}
+            {/* <video className="base-img" autoPlay playsinline muted preload="auto">
+                <source src={src} type="video/mp4" />
+            </video> */}
+            <Image src={singlepagedata.image} w={"100%"} />
 
 
-    <VStack display="flex" margin="auto" direction='column'
-     mr="50%" position="absolute" mt ="-100px" ml="70px"
-    
-     >
-        
-        <Button style={style} >Play Free Episode</Button>
-        <Button style={style} >Accept 3 Months Free</Button>
-    </VStack>
+            <VStack display="flex" margin="auto" direction='column'
+                mr="50%" position="absolute" mt="-100px" ml="70px"
+
+            >
+
+                <Button style={style} >Play Free Episode</Button>
+                <Button style={style} >Accept 3 Months Free</Button>
+            </VStack>
 
 
-<Box height="25px" width ="auto"  border="1px solid transparent"  ml ="250px" position="absolute"
-mt="-100px" color="white"
->
- S1 E1 :  {singlepagedata.title}
-</Box>
+            <Box height="25px" width="auto" border="1px solid transparent" ml="250px" position="absolute"
+                mt="-100px" color="white"
+            >
+                S1 E1 :  {singlepagedata.title}
+            </Box>
 
 
-<Box>
-<Carausel  data ={singlepagedata.episode} />
-</Box>
+            <Box>
+                <Carausel data={singlepagedata.episode} />
+            </Box>
 
-<Box>
-    <Cast  id={id} />
+            <Box>
+                <Cast id={id} />
 
 
 
-</Box>
-   </Box>
-      
+            </Box>
+        </Box>
 
-    
-  )
+
+
+    )
 }

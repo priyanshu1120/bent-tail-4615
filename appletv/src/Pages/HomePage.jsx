afef9ab4229d1b5b046c82c7207911a16a8667e4
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-11-12 17:26:52 +0530

    base  image added

--- appletv/src/Pages/HomePage.jsx
+++ appletv/src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Box,Text } from '@chakra-ui/react'
+import { Box,Img,Text } from '@chakra-ui/react'
 import React from 'react'
 import { Comedy } from '../component/comedy'
 import Faq from '../Components/Faq'
@@ -46,12 +46,16 @@ const HomePage = () => {
       <CommonSlider url="https://appletv-server-cljhqn6xz-nrishav007.vercel.app/futureRelease"  />
       </Box>
 
-      <Box mt="15px">
+      <Box mt={"20px"} >
+      <Box>
+      <Img  src="https://is4-ssl.mzstatic.com/image/thumb/Features122/v4/b5/aa/57/b5aa5705-1a39-af37-805b-257af57ad42a/db917410-1e16-4f3a-96f6-4f11a8f78fa2.png/2638x988sr.webp" alt="img"/>
+      </Box>
+      <Box >
       <Text fontSize="22px" fontWeight={700} pl="40px">Future Releases</Text>
       <Text fontSize="19px" fontWeight={400} pl="40px">Add to Up Next now</Text>
       <StaticSlider2  data = { sliderdata2 } />
       </Box>
-
+      </Box>
      {/* <=========videospace========> */}
 
 
@@ -86,6 +90,21 @@ const HomePage = () => {
       <Box>
       <Static/>
      </Box>
+     <Box mt="60px">
+      <Text pt="10px" fontSize="22px" fontWeight={700} pl="40px">All Features Film </Text>
+      <CommonSlider url="https://appletv-server-cljhqn6xz-nrishav007.vercel.app/mostPopular"  />
+      </Box>
+
+      <Box mt="15px">
+      <Text pt="10px" fontSize="22px" fontWeight={700} pl="40px">All Darama Series </Text>
+      <CommonSlider url="https://appletv-server-cljhqn6xz-nrishav007.vercel.app/allFeatureFilms"  />
+      </Box>
+        
+      <Box mt="15px">
+      <Text pt="10px" fontSize="22px" fontWeight={700} pl="40px">Entire Originals</Text>
+      <CommonSlider url="https://appletv-server-cljhqn6xz-nrishav007.vercel.app/watchPremiers"  />
+      </Box>
+
       <Faq/>
     </div>
   )

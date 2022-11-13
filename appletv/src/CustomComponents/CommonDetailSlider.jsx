bcf8c7188e6395f221120d68beba8bbdc791b636
IAkashMondal <adas085322gmail.com>
2022-11-13 18:49:44 +0530

    final

--- appletv/src/CustomComponents/CommonDetailSlider.jsx
+++ appletv/src/CustomComponents/CommonDetailSlider.jsx
@@ -115,7 +115,7 @@ function CommonDetailSlider({ url }) {
   return (
     <div className="slider4">
       <Slider {...settings}>
-        {data.map((item, i) => (
+        {data.map((item) => (
           <div className="common-detail-slider-card" key={item.id}>
             <div className="common-detail-slider-card-top">
               <Link to={`/${pathurl}/${item.id}`}>

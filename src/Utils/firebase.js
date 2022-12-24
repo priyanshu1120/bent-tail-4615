06592baf6a46dd171895e7293b7b799fd11fd666
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-12-24 16:19:00 +0530

    firebase utils updated for deployement

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -4,13 +4,13 @@ import{getAuth, GoogleAuthProvider} from "firebase/auth"
 import{getStorage} from "firebase/storage"
 import{getFirestore} from "firebase/firestore"
 export const firebaseConfig = {
-  apiKey: "AIzaSyBfYXAB8Rh5aegw0pGi9aPabxkSxrL76MM",
-  authDomain: "my-app-tv-user.firebaseapp.com",
-  projectId: "my-app-tv-user",
-  storageBucket: "my-app-tv-user.appspot.com",
-  messagingSenderId: "206951734006",
-  appId: "1:206951734006:web:10e2d09b0c472ae290ead7",
-  measurementId: "G-7GV22D87RT"
+  apiKey: "AIzaSyBcSRk1li7bGtE0hLu3jM7-tI2WYphcck0",
+  authDomain: "mytv-e1b89.firebaseapp.com",
+  projectId: "mytv-e1b89",
+  storageBucket: "mytv-e1b89.appspot.com",
+  messagingSenderId: "6861621605",
+  appId: "1:6861621605:web:0ed390d1b02e23d625398c",
+  measurementId: "G-HKH8VHWXZH"
 };
 
 // Initialize Firebase
